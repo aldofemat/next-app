e9470ab40d1ab4adc1d0048181ad5d27bdcce9f8
aldofemat <ALDO.AGUILAR.FEMATT@GMAIL.COM>
2023-08-03 10:58:28 -0600

    se agrego meta sin head en layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,9 +3,9 @@
 import "@styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Head from "next/head";
+import { Metadata } from 'next'
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>;
+
 
 export const metadata = {
   title: "Aldo Femat Fitness App",
@@ -15,8 +15,8 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="es">
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <body>
           <div className="w-400 mx-auto xl:w-1488">
             <Navbar />
@@ -24,7 +24,7 @@ const RootLayout = ({ children }) => {
             <Footer />
           </div>
         </body>
-      </Head>
+      
     </html>
   );
 };
