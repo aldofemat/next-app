1f7660f8686ffe7fae1b008652b39d0d70487eb7
aldofemat <ALDO.AGUILAR.FEMATT@GMAIL.COM>
2023-08-03 04:11:33 -0600

    px=20px en footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#000000">
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="20px" pt="24px">
      {/*  <Image src={Logo} alt="logo" color='#000000' style={{ width: '500px', height: '41px' }} /> */}
     </Stack>
     <Typography variant="h5" color="#FFFAFB" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Hecho con ❤️ por Aldo Femat</Typography>
