397094d305a2578787e68a0304a01c9539e245ba
aldofemat <ALDO.AGUILAR.FEMATT@GMAIL.COM>
2023-08-03 03:24:17 -0600

    se quito imagen footer y se actualizo imagen navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 import Link from "next/link";
 import { Stack } from '@mui/material';
 import Image from 'next/image';
-import Logo from "@public/assets/icons/AldoFematLogo1.png";
+import Logo from "@public/assets/icons/AldoFematLogoTransparente.png";
 
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around"  sx={{ gap: { sm: '122px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none'}} px="20px">
 
     <Link href='/' className='flex gap-2 flex-center'>
 
-      <Image src={Logo} alt="logo" style={{ width: '100px', height: '100px', margin: '0px 20px' }} />
+      <Image src={Logo} alt="logo" style={{ width: '80px', height: '45px', margin: '0px 20px' }} />
     
     </Link>
 
