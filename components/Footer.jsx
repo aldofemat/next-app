5390760b2bfdbe736f575fca4317cc5029de95bc
aldofemat <ALDO.AGUILAR.FEMATT@GMAIL.COM>
2023-08-03 20:23:34 -0600

    se cambio el gap a xs:15px

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => (
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+    <Stack gap="0px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="10px" pt="24px">
      {/*  <Image src={Logo} alt="logo" color='#000000' style={{ width: '500px', height: '41px' }} /> */}
     </Stack>
     <Typography variant="h5" color="#FFFAFB" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Hecho con ❤️ por Aldo Femat</Typography>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import Logo from "@public/assets/icons/AldoFematLogoTransparente.png";
 
 const Navbar = () => (
-  <Stack direction="row" justifyContent="space-around"  sx={{ gap: { sm: '122px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none'}} px="0px">
+  <Stack direction="row" justifyContent="space-around"  sx={{ gap: { sm: '122px', xs: '15px' }, mt: { sm: '32px', xs: '15px' }, justifyContent: 'none'}} px="10px">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
